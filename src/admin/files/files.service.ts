83db8527516d8f99bb19b787ecff4b37698c3b51
iosebi1999 <iosebi@hlwgroup.dev>
2025-03-07 13:03:44 +0400

    Removed all

--- src/admin/files/files.service.ts
+++ src/admin/files/files.service.ts
@@ -238,16 +238,4 @@ export class FilesService {
       count,
     };
   }
-
-  findOne(id: number) {
-    return `This action returns a #${id} file`;
-  }
-
-  update(id: number, updateFileDto: UpdateFileDto) {
-    return `This action updates a #${id} file`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} file`;
-  }
 }
